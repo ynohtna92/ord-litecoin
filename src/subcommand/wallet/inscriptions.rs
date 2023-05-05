8d297e33bc9daf1a8c7593eb6aa4b1b54a75409b
daidean <ddiq@msn.com>
2023-05-05 13:13:08 +0800

    Get the correct inscription link

--- src/subcommand/wallet/inscriptions.rs
+++ src/subcommand/wallet/inscriptions.rs
@@ -15,10 +15,10 @@ pub(crate) fn run(options: Options) -> Result {
   let unspent_outputs = index.get_unspent_outputs(Wallet::load(&options)?)?;
 
   let explorer = match options.chain() {
-    Chain::Mainnet => "https://ordinals.com/inscription/",
+    Chain::Mainnet => "https://ordinalslite.com/inscription/",
     Chain::Regtest => "http://localhost/inscription/",
-    Chain::Signet => "https://signet.ordinals.com/inscription/",
-    Chain::Testnet => "https://testnet.ordinals.com/inscription/",
+    Chain::Signet => "https://signet.ordinalslite.com/inscription/",
+    Chain::Testnet => "https://testnet.ordinalslite.com/inscription/",
   };
 
   let mut output = Vec::new();
